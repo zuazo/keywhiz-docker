03cf4d61fcd7e2eaee42cabea87a030bf5da7e0a
Xabier de Zuazo <xabier@zuazo.org>
2015-12-12 08:57:28 +0100

    Use dockerspec gem for tests

--- Gemfile
+++ Gemfile
@@ -3,10 +3,4 @@
 
 source 'https://rubygems.org'
 
-gem 'rake'
-
-gem 'docker-api', '~> 1.21'
-gem 'serverspec', '~> 2.19'
-
-# You should_not start your specs with the string "should"
-gem 'should_not', '~> 1.1'
+gem 'dockerspec', '~> 0.2.0'
---
+++ spec/keywhiz_spec.rb
@@ -0,0 +1,34 @@
+require 'dockerspec'
+require 'dockerspec/serverspec'
+
+describe docker_build('.', tag: 'keywhiz') do
+  it { should have_entrypoint '/entrypoint.sh' }
+  it { should have_workdir %r{^/opt/keywhiz-} }
+  it { should have_expose '4444' }
+  it { should have_env 'KEYWHIZ_VERSION' }
+
+  describe docker_run('keywhiz') do
+    before(:all) { sleep(120) if ENV.key?('CI') }
+
+    describe user('keywhiz') do
+      it { should exist }
+    end
+
+    describe group('keywhiz') do
+      it { should exist }
+    end
+
+    describe process('java') do
+      it { should be_running }
+      its(:user) { should eq 'keywhiz' }
+      its(:args) { should match /keywhiz-server-shaded\.jar/ }
+    end
+
+    describe port(4444) do
+      it do
+        sleep(10)
+        should be_listening
+      end
+    end
+  end
+end
