926b357c6046e5f1c9214a2b4225e77f81d914c9
Xabier de Zuazo <xabier@zuazo.org>
2015-12-13 08:58:45 +0100

    Tests: run a health check on admin connector

--- spec/keywhiz_spec.rb
+++ spec/keywhiz_spec.rb
@@ -1,5 +1,6 @@
 require 'dockerspec'
 require 'dockerspec/serverspec'
+require 'json'
 
 describe docker_build('.', tag: 'keywhiz') do
   it { should have_entrypoint '/entrypoint.sh' }
@@ -9,6 +10,8 @@ describe docker_build('.', tag: 'keywhiz') do
 
   describe docker_build(File.dirname(__FILE__), tag: 'keywhiz_test') do
     describe docker_run('keywhiz_test') do
+      before(:all) { sleep(10) }
+
       describe user('keywhiz') do
         it { should exist }
       end
@@ -23,11 +26,29 @@ describe docker_build('.', tag: 'keywhiz') do
         its(:args) { should match(/keywhiz-server-shaded\.jar/) }
       end
 
-      # Does not work on CircleCI
       describe port(4444), if: !ENV.key?('CIRCLECI') do
-        it do
-          sleep(10)
-          should be_listening
+        it { should be_listening }
+      end
+
+      describe port(8085), if: !ENV.key?('CIRCLECI') do
+        it { should be_listening }
+      end
+
+      describe command('wget -O- 127.0.0.1:8085/ping') do
+        its(:stdout) { should match /^pong/ }
+      end
+
+      describe command('wget -O- 127.0.0.1:8085/healthcheck') do
+        let(:json) { JSON.parse(subject.stdout) }
+
+        it 'returns a valid JSON' do
+          json
+        end
+
+        %w(db-read-write-health db-readonly-health deadlocks).each do |field|
+          it "#{field} is healthy" do
+            expect(json[field]['healthy']).to be true
+          end
         end
       end
     end
